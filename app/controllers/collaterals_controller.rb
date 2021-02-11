590542380acf004148fd683e32659d50f88a0f7a
malgorzatamaz <m.mazurek@selleo.com>
2021-02-11 18:46:30 +0100

    working adding of collateral

--- app/controllers/collaterals_controller.rb
+++ app/controllers/collaterals_controller.rb
@@ -8,8 +8,29 @@ class CollateralsController < ApplicationController
   end
 
   def new
+    @collateral_types = {
+      0 => 'Article',
+      1 => 'Video',
+      2 => 'Podcast',
+      3 =>  'Design',
+     }
+
     @collateral = Collateral.new()
     @tags = Tag.all
+  end
+
+  def create
+    @collateral = Collateral.new()
+    @collateral.name = params[:name]
+    @collateral.description = params[:description]
+    @collateral.url = params[:url]
+    @collateral.url = params[:collateral_type]
+
+    if @collateral.save 
+      redirect_to "/collaterals/#{@collateral.id}"
+    else
+      render "new"
+    end
   end
 
   def edit
