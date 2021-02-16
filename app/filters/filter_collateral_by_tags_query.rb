750590e631ee3e04f5e4910613100b7b9825b251
Kajetan Boruta <kajetan.boruta@bosmal.com.pl>
2021-02-16 17:37:01 +0100

    fix test env

--- app/filters/filter_collateral_by_tags_query.rb
+++ app/filters/filter_collateral_by_tags_query.rb
@@ -6,9 +6,9 @@ class FilterCollateralByTagsQuery
   def result
     Collateral.includes(collaterals_tags: :tag).where(collaterals_tags: {tag_id: @tags}).map do |collateral|
       { collateral: collateral, weight: collateral.collaterals_tags.sum do |collateral_tag| 
-        ((@tags.reverse.index(collateral_tag.tag_id) + 1).to_f / @tags.length.to_f) * collateral_tag.weight
+        ((@tags.reverse.index(collateral_tag.tag_id).to_f + 1).to_f / @tags.length.to_f) * collateral_tag.weight
         end
       }
-    end.sort_by { |k| - k[:weight] }
+    end.sort_by { |k| k[:weight] }.reverse
   end
 end
