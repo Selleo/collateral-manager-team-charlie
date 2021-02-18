d9e1228c6737a83955cf5a74b02e76f2faad5483
malgorzatamaz <m.mazurek@selleo.com>
2021-02-18 17:43:35 +0100

    searching

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -61,51 +61,23 @@ class LeadsController < ApplicationController
     redirect_to "/leads"
   end
 
-  #function to show collaterals
   def show
     @lead = Lead.find(params[:id])
     @selected_tags_id = @lead.tags.map(&:id)
-
-    # @collaterals = collateral.all
-    @collaterals = Collateral.includes(:tags).where(tags: {id: @selected_tags_id})
+    @result = FilterCollateralByTagsQuery.new(@selected_tags_id).result
+    @collaterals = @result.map { |item| item[:collateral] }
   end
 
-  #Function to show collaterals by specific tags, 
-  #if user chooses tags [ruby, js] it will find only those, which contain at least [ruby,js] 
-  def show_by_tags
-    @specific_tags = [1]
-    Collateral.includes(collaterals_tags: :tag).where(collaterals_tags: {tag_id: @specific_tags}).map do |collateral|
-      { collateral: collateral, weight: collateral.collaterals_tags.sum do |collateral_tag| 
-        ((@specific_tags.reverse.index(collateral_tag.tag_id) + 1).to_f / @specific_tags.length.to_f) * collateral_tag.weight
-        end
-      }
-    end.sort_by { |k| - k[:weight] }
-
+  def get_collaterals
+    tags = params["tags"]
+    if tags != nil
+      array_of_tags = tags.map(&:to_i)
+      found_collaterlas = FilterCollateralByTagsQuery.new(array_of_tags).result
+      render json: found_collaterlas
+    end
   end
 
+  def leads_params
+    params.require(:lead).permit(:name)
+  end
 end
-
-def leads_params
-  params.require(:lead).permit(:name)
-end
-
-# Collateral.includes(collaterals_tags: :tag).where(collaterals_tags: {tag_id: @specific_tags}).map {|a| 1}
-
-
-
-# working one
-    # Collateral.includes(collaterals_tags: :tag).where(collaterals_tags: {tag_id: @specific_tags})
-    #   .map {
-    #     |collateral| collateral.collaterals_tags
-    #       .map { 
-    #         |collateral_tag| ((@specific_tags.reverse.index(collateral_tag.tag_id) + 1).to_f / @specific_tags.length.to_f) * collateral_tag.weight
-    #       }.sum
-    #   }
-
-
-    # Collateral.includes(collaterals_tags: :tag).where(collaterals_tags: {tag_id: @specific_tags}).map do |collateral|
-    #   { collateral: collateral, weight: collateral.collaterals_tags.sum do |collateral_tag| 
-    #     ((@specific_tags.reverse.index(collateral_tag.tag_id) + 1).to_f / @specific_tags.length.to_f) * collateral_tag.weight
-    #     end
-    #   }
-    # end.sort_by! { |k| k[:weight] * -1 }
