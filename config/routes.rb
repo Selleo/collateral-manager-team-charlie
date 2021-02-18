1454881daef60b796aa607c772de8940640f2504
Adam Nowok <78403731+meOffA@users.noreply.github.com>
2021-02-18 16:55:19 +0100

    Revert "Searching by tag"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => redirect('/collaterals')
-  post 'lead/get_collaterals' => 'lead#get_collaterals'
+
   devise_for :users
   resources :leads
   resources :tag
