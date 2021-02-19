d6cb4d755339e8bdc1a4b7661e61cca41849151f
Kajetan Boruta <kajetan.boruta@bosmal.com.pl>
2021-02-19 14:46:40 +0100

    hotfix lead controller spec

--- spec/controllers/lead_controller_spec.rb
+++ spec/controllers/lead_controller_spec.rb
@@ -1,26 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe LeadController, type: :controller do
+RSpec.describe LeadsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #new" do
     it "returns http success" do
       get :new
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(302)
     end
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+      lead = create(:lead)
+      get :show, params: {id: lead.id}
+      expect(response).to have_http_status(302)
     end
   end
 
+  describe "GET #edit" do
+    it 'returns http success' do
+      lead = create(:lead)
+      get :show, params: {id: lead.id}
+      expect(response).to have_http_status(302)
+    end
+  end
 end
